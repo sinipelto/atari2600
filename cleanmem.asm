53ccd987998721a4868ed25956b27e432fd20a37
Sinipelto <toni.blafield@tuni.fi>
2021-05-23 14:08:22 +0300

    remove bin. fix cleanmem to clean $00

--- cleanmem.asm
+++ cleanmem.asm
@@ -1,24 +1,28 @@
-    processor 6502 ; define used processor chip
+    processor 6502      ; define used processor chip
 
-    seg code ; segment "code"
-    org $F000   ; start address
+    seg code            ; segment "code"
+    org $F000           ; start address
 
 Start:
-    sei     ; disable interrupts flag = 0
-    cld     ; clear decimal flag 
-    ldx #$FF    ; loads into X value FF
-    txs     ; transfer X to SP
+    sei                 ; disable interrupts flag = 0
+    cld                 ; clear decimal flag 
+    ldx #$FF            ; loads into X value FF
+    txs                 ; transfer X to SP
 
-;;;;;;;;;;;;;;;;;;; Clear mem from $00 to $ff ;;;;;;;;;;;;;;;;;;;;;;;;;;;
-    lda #0      ; A = 0
-    ldx #$FF    ; X = FF
+;;;;;;;;;;;;;;;;;;; Clear mem from $FF to $00 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    lda #0              ; A = 0
+    ldx #$FF            ; X = FF
+    ldy #0              ; Y = 0 (fixes crash occurring by sta $FF)
+
+    ; Initially set mem 0xFF to 0
+    sta $FF             ; Store A(=0) into mem pos $FF
 
 MemLoop:
-    sta $0,x       ; Store A into $0 + X
-    dex             ; X--
-    bne MemLoop     ; while Z != 0
+    dex                 ; X--
+    sta $0,x            ; Store A into $0 + X
+    bne MemLoop         ; while X != 0
 
 ;;;;;;;;;;;;;;;;; Fill rom size 4kB ;;;;;;;;;;;;;;;;;;;;;;;
-    org $FFFC       ; Set pos to cartrigde END
-    .word Start     ; Start -> Reset vector => FFFC
-    .word Start     ; Start once more (interrupt) => FFFE
+    org $FFFC           ; Set pos to cartrigde END
+    .word Start         ; Start -> Reset vector => FFFC
+    .word Start         ; Start once more (interrupt) => FFFE

111615e993e0ba1ec946d8ea11d92bd71f719c21
Toni Blåfield <toni.blafield@etteplanmore.com>
2021-05-30 12:36:37 +0300

    bomber: display draw, bgcolor.

--- bomber.asm
+++ bomber.asm
@@ -16,14 +16,55 @@ Var  .byte     ; RAM variable
      ORG $F000
 
 ;;; reset vcs REGS, RAM, TIA ;;;
+;;; game reset endpoint ;;;
 Reset:
      CLEAN_START
 
 ;;;; init variables ;;;;
-     LDX #0
-     STA Var
+Start:
+     LDA #$82
+     STA COLUBK     ; set BG blue
+
+     LDX #2
+     LDY #0
+
+Frame:
+;;;; VSYNC ;;;;
+     STX VBLANK
+     STX VSYNC
+
+     REPEAT 3
+          STA WSYNC
+     REPEND
+
+     STY VSYNC
+
+;;;; VBLANK ;;;;
+     REPEAT 37
+          STA WSYNC
+     REPEND
+
+     STY VBLANK
+
+;;;; Visible picture ;;;;
+     REPEAT 192
+          STA WSYNC
+     REPEND
+
+;;;; Overscan ;;;;
+     STX VBLANK
+
+     REPEAT 30
+          STA WSYNC
+     REPEND
+
+     STY VBLANK
+
+;;;; Loop next frame ;;;;
+     JMP Frame
 
 ;;;; Define rom end ;;;;
      ORG $FFFC
-     .word Reset
-     .word Reset    ; rom launch endpoint
+     .word Reset    ; FFFC,FFFE
+     .word Reset    ; FFFE,FFFF rom launch endpoint
+     ; at FFFF
